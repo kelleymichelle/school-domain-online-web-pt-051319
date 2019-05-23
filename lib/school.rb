8a8369cca06d97207d99aadc4db11afb2f099203
Kelley Chaplain <kmchaplain@gmail.com>
2019-05-22 23:08:08 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+require 'pry'
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end   
+
+    def add_student(student, grade)
+            if @roster[grade]
+            @roster[grade] << student
+        else     
+            @roster[grade] = []
+            @roster[grade] << student    
+        end            
+    end
+    
+    def grade(num)
+        return @roster[num]
+    end
+    
+    def sort
+        order = {}
+       @roster.sort.to_h.map do |grade, students|
+        order[grade] = []
+          order[grade] = students.sort
+       end
+       return order
+    end    
+
+
+
+
+
+
+end    
